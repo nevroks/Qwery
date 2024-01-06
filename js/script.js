c24bb6c60318ca7af923593ff12763745c919dfa
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-07 00:50:15 +0500

    start working on slider but i need to go for sleep

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ document.querySelector(".Scroll-down__button").addEventListener("click", () => {
 })
 
 ///----------------------------
+/// Slider script--------------
+let offset = 0
+const sliderLine = document.querySelector('.slider__line')
+document.querySelector('.slider__next-btn').addEventListener('click', () => {
+    if (offset === 200) {
+        offset = 0
+    } else {
+        offset = offset + 100
+    }
+    sliderLine.style.right = offset + '%'
+})
+document.querySelector('.slider__prev-btn').addEventListener('click', () => {
+        if (offset <= 0) {
+            offset = 200
+        } else {
+            offset = offset - 100
+        }
+        sliderLine.style.right = offset + '%'
+    })
+    ///----------------------------
