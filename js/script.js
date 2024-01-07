a96636f950b45af935557eac37c845a176672f57
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-07 15:37:50 +0500

    ended slider

--- js/script.js
+++ js/script.js
@@ -13,19 +13,19 @@ document.querySelector(".Scroll-down__button").addEventListener("click", () => {
 let offset = 0
 const sliderLine = document.querySelector('.slider__line')
 document.querySelector('.slider__next-btn').addEventListener('click', () => {
-    if (offset === 200) {
+    if (offset === 2962) {
         offset = 0
     } else {
-        offset = offset + 100
+        offset = offset + 1481
     }
-    sliderLine.style.right = offset + '%'
+    sliderLine.style.right = offset + 'px'
 })
 document.querySelector('.slider__prev-btn').addEventListener('click', () => {
         if (offset <= 0) {
-            offset = 200
+            offset = 2962
         } else {
-            offset = offset - 100
+            offset = offset - 1481
         }
-        sliderLine.style.right = offset + '%'
+        sliderLine.style.right = offset + 'px'
     })
     ///----------------------------
