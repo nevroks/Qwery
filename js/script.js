68f0071795a7d46d503cc1020ed0c0cc59f3fbef
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-09 16:29:07 +0500

    added some response but not finished yet

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /// Scroll down script---------
 const sectionToScroll = document.querySelector(".location1")
 document.querySelector(".Scroll-down__button").addEventListener("click", () => {
-    console.log("clicked");
     sectionToScroll.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
